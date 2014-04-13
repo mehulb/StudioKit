01984a1609299848f22d8001d15cc2f02bc9b6a6
Mehul Bhavani <mehul.bhavani@9pstudio.com>
2014-04-13 12:02:23 +0530

    initial commit

---
+++ StudioKit/StudioKit/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  StudioKit
+//
+//  Created by Mehul Bhavani on 13/04/14.
+//  Copyright (c) 2014 9P Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StudioKit/StudioKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StudioKit
+//
+//  Created by Mehul Bhavani on 13/04/14.
+//  Copyright (c) 2014 9P Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SKAppDelegate class]));
+    }
+}
