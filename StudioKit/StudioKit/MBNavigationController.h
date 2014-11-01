c4529983a4328095d5db6a7b50f048abeb19916f
Mehul Bhavani <mehul.bhavani@9pstudio.com>
2014-11-01 16:56:06 +0530

    added navctrl

---
+++ StudioKit/StudioKit/MBNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  MBNavigationController.h
+//  cloudnine
+//
+//  Created by Mehul Bhavani on 22/08/14.
+//  Copyright (c) 2014 PocketMedi Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBNavigationController : UINavigationController
+
+@end
---
+++ StudioKit/StudioKit/MBNavigationController.m
@@ -0,0 +1,58 @@
+//
+//  MBNavigationController.m
+//  cloudnine
+//
+//  Created by Mehul Bhavani on 22/08/14.
+//  Copyright (c) 2014 PocketMedi Inc. All rights reserved.
+//
+
+#import "MBNavigationController.h"
+
+@interface MBNavigationController ()
+
+@end
+
+@implementation MBNavigationController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)loadView
+{
+    [super loadView];
+    
+    UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0, 44, SCREEN_BOUNDS.size.width, 1)];
+    lineView.backgroundColor = ThemeColorPrimaryPink;
+    
+    [self.navigationBar addSubview:lineView];
+}
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
